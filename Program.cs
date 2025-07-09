3b41a7e3b69d1a4d8fc09421b711bf1b0b3bda73
alex <alexandr.paramon56@gmail.com>
2025-07-09 15:22:02 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Write_from_csv_to_database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
