c882a825b9853faab792b4668afe9deb41dc4909
alex <alexandr.paramon56@gmail.com>
2025-07-17 22:42:08 +0500

    feat(json-serializer): Implemented JSON serializer

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,18 +1,21 @@
-﻿using Write_from_csv_to_database.Dependencies.Attributes;
+﻿using System.Runtime.Serialization;
+using Write_from_csv_to_database.Dependencies.Attributes;
 using Write_from_csv_to_database.Dependencies.Mappings;
 
 namespace Write_from_csv_to_database.Models;
 
 [CsvMap(typeof(PetMap))]
-public record class Pet
+[DataContract]
+public record class Pet : BaseModel
 {
+    [DataMember]
     public int Id { get; set; }
-
+    [DataMember]
     public string? Name { get; set; }
-
+    [DataMember]
     public Species Species { get; set; }
-
+    [DataMember]
     public int OwnerId { get; set; }
-
+    [DataMember]
     public Person? Owner { get; set; }
 }
